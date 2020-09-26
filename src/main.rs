73a94f1d2a96f93923fa52b96cd310d0ca46ad59
Tomasz Zema <40647700+T-Zema@users.noreply.github.com>
2020-09-26 08:37:02 +0200

    its a joke

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+#![allow(dead_code)]
+#![allow(non_snake_case)]
+#![allow(unused_imports)]
+#![allow(unused_variables)] 
+//#![allow(unused_brain)] 
+
 fn main() {
     println!("Hello, world!");
 }
